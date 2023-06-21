635a47670a69d8db968d0b14f200f6a01e554304
Marco <dontstop.marco@gmail.com>
2023-06-21 23:15:40 +0200

    first route implementation

--- tests/Feature/ProductUpload.php
+++ tests/Feature/ProductUpload.php
@@ -3,18 +3,68 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ProductUpload extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    private array $header;
+    private array $csvData;
+    private $csvFile;
+
+    public function setUp(): void
+    {
+
+        //create fake data to put into csv file
+        $this->header = ["id","name","price"];
+        $this->csvData = [
+            [
+                Str::uuid(),
+                'product 1',
+                 3.99
+            ],
+            [
+                Str::uuid(),
+                'product 2',
+                16
+            ],
+            [
+                Str::uuid(),
+                'name' => 'new product',
+                'price' => 11.60
+            ]
+
+        ];
+        parent::setUp();
+    }
+
+    public function test_should_upload_csv_file(): void
+    {
+        // Arrange
+        Storage::fake('csv');
+        $file = UploadedFile::fake()->create('test.csv');
+
+        //append csv rows
+        $handle = $file->openFile('a');
+        $handle->fputcsv($this->header);
+        foreach ($this->csvData as $csvRow) {
+            $handle->fputcsv($csvRow);
+        }
+
+        // Act
+        $response = $this->post('/upload-products', []);
+
+        var_dump($response);
+
+        // Assert that
+        Storage::disk('csv')->assertExists($file->hashName());
+
+        # Check if you have any DB entry against that
+        $this->assertDatabaseCount('products', count($this->csvData));
     }
 }
